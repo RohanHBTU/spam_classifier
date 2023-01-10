d44d4c04ba0f51b2231b4d88cbf4403384f4d7b1
Rohan Kumar Singh <97730338+RohanHBTU@users.noreply.github.com>
2023-01-10 18:36:20 +0530

    included nltk stopwords

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 nltk.download('punkt')
+nltk.download('stopwords')
 
 #tfidf = pickle.load(open('vectorizer.pkl','rb'))
 #model = pickle.load(open('model.pkl','rb'))
